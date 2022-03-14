403fbdb63bd1eba9f1abfe14537308bc2926d75f
FlorianBEME <florianbme@gmail.com>
2022-03-14 16:01:06 +0100

    push

--- challenge.ts
+++ challenge.ts
@@ -14,7 +14,7 @@ const prettyPrintWilder = (users: User[]): void => {
 
 const wilders: User[] = [];
 const user1: User = { name: "Pierre", age: 23 };
-const user2: User = { name: "Paul", birthday: "10/02/1990" };
+const user2: User = { name: "Paul", age: 32, birthday: "10/02/1990" };
 const user3: User = { name: "Jacques", age: 25 };
 wilders.push(user1);
 wilders.push(user2);
